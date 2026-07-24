b263a206dbb62ac4c0cf417b531da94ada4f7c96
mujib38 <68101025+mujib38@users.noreply.github.com>
2026-07-25 02:07:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-// Vacation Planner Website
-console.log("Website Loaded Successfully");
+const swiper = new Swiper(".heroSwiper", {
+
+    loop: true,
+
+    speed: 1000,
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    effect: "slide",
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
