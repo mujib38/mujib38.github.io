2f15e583c5e9b9572f6ad9896f77f0aae58ed8a4
mujib38 <68101025+mujib38@users.noreply.github.com>
2026-07-26 13:16:52 +0530

    Fix formatting issues in swiper initialization

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 const swiper = new Swiper(".heroSwiper", {
-
     loop: true,
-
     speed: 500,
-
     autoplay: {
         delay: 2000,
         disableOnInteraction: false,
     },
-
     effect: "slide",
-
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-
 });
